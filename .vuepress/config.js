f26667c63c03294197588710f10ad44250eb71d9
himself65 <himself65@outlook.com>
2019-07-04 11:58:25 +0800

    feat: add toolbar links

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   title: 'LianXue',
   description: 'the Cre\'s love science',
-  base: '/LianXue/'
+  base: '/LianXue/',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '恋学实践', link: '/r18/' },
+      { text: 'GitHub', link: 'https://github.com/himself65/LianXue' },
+    ]
+  }
 };
