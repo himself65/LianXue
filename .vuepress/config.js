0a3ac3947298e6499e13dfad10dad765639a3ac6
ICEY <icey@icey.tech>
2019-08-16 00:57:37 +0800

    增加恋学程序设计语言相关部分

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
       { text: '恋学实践', link: '/r18/' },
       { text: '推论证明', link: '/proof/' },
       { text: 'Chuigda之卷轴', link: '/chuigda-reel/' },
+      { text: 'Love-Lang', link: '/love-lang/' },
       { text: 'GitHub', link: 'https://github.com/himself65/LianXue' },
     ]
   }
